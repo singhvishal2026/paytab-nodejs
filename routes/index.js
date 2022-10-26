0ccc24fe48bbd216bad10393d865669134e00143
Vishal Singh <vishal.singh@appinventiv.com>
2022-10-26 16:45:06 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,38 @@ router.get("/", function (req, res, next) {
 });
 router.post("/payment", async function (req, res, next) {
   console.log("re", req.body);
+  let payload={
+    profile_id: "92790",
+    cart_amount: req.body.amount,
+    cart_currency: "SAR",
+    cart_description: "watani booking amount",
+    cart_id: `WA${Math.floor(Math.random() * 10000000) + 1}`, //use any uuid
+    tran_type: "sale",
+    tran_class: "ecom",
+    customer_details: {
+      name: req.body.name,
+      email: req.body.email,
+      street1: "404, 11th st, void",
+      city: "Dubai",
+      state: "DU",
+      country: "AE",
+      ip: "91.74.146.168",  
+    },
+    payment_token: req.body.token,
+    return: process.env.PAYTAB_REDIRECT_URL, // pick from env, browser redirect url afer success of 3ds
+    callback:process.env.PAYTAB_CALLBACK_URL // post request from paytab server to our server with transaction details 
+  }
+
+  if(req.body.saveCard==true)
+  {
+    payload.tokenise=2;
+    // payload.tran_class='recurring';
+  }
+
   try {
     let trns = await axios.post(
       "https://secure.paytabs.sa/payment/request",
-      {
-        profile_id: "92790",
-        cart_amount: req.body.amount,
-        cart_currency: "SAR",
-        cart_description: "watani booking amount",
-        cart_id: `WA${Math.floor(Math.random() * 10000000) + 1}`, //use any uuid
-        tran_type: "sale",
-        tran_class: "ecom",
-        customer_details: {
-          name: req.body.name,
-          email: req.body.email,
-          street1: "404, 11th st, void",
-          city: "Dubai",
-          state: "DU",
-          country: "AE",
-          ip: "91.74.146.168",  
-        },
-        payment_token: req.body.token,
-        return: process.env.PAYTAB_REDIRECT_URL, // pick from env, browser redirect url afer success of 3ds
-        callback:process.env.PAYTAB_CALLBACK_URL // post request from paytab server to our server with transaction details 
-      },
+      payload,
       {
         headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
       }
@@ -46,6 +54,50 @@ router.post("/payment", async function (req, res, next) {
 router.post("/updateTransactionStatus", async function (req, res, next) {
   console.log('updated transaction output',req.body);
   res.status(200).send();
+});
+
+router.post("/hosted-payment", async function (req, res, next) {
+  console.log("re", req.body);
+  let payload={
+    profile_id: "92790",
+    cart_amount: req.body.amount,
+    cart_currency: "SAR",
+    cart_description: "watani booking amount",
+    cart_id: `WA${Math.floor(Math.random() * 10000000) + 1}`, //use any uuid
+    tran_type: "sale",
+    tran_class: "ecom",
+    hide_shipping: true,
+    framed: true,
+    tokenise: 2,
+    show_save_card: true,
+    config_id: 2188,
+    paypage_lang: "en",
+    customer_details: {
+      name: req.body.name,
+      email: req.body.email,
+      street1: "404, 11th st, void",
+      city: "Dubai",
+      state: "DU",
+      country: "AE",
+      ip: "91.74.146.168",  
+    },
+    return: process.env.PAYTAB_REDIRECT_URL, // pick from env, browser redirect url afer success of 3ds
+    // callback:process.env.PAYTAB_CALLBACK_URL // post request from paytab server to our server with transaction details 
+  }
+
+  try {
+    let trns = await axios.post(
+      "https://secure.paytabs.sa/payment/request",
+      payload,
+      {
+        headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
+      }
+    );
+    res.send({message:"Success",result:trns.data,statusCode:200});
+  } catch (error) {
+    res.status(400).send({message:"Failure",result:error,statusCode:400});
+    console.log("error", error);
+  }
 });
 
 module.exports = router;
