d0c27da8fd130ee25c01f53032ef93b62e0868f5
Vishal Singh <vishal.singh@appinventiv.com>
2022-10-27 10:26:59 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get("/", function (req, res, next) {
 router.post("/payment", async function (req, res, next) {
   // Store initial transaction details in db mark it as pending state
   // Add user id & own db transaction id into user defined field
-  let payload={
+  let payload = {
     profile_id: process.env.PAYTAB_PROFILE_ID,
     cart_amount: req.body.amount,
     cart_currency: "SAR",
@@ -23,16 +23,21 @@ router.post("/payment", async function (req, res, next) {
       city: "Dubai",
       state: "DU",
       country: "AE",
-      ip: "91.74.146.168",  
+      ip: "91.74.146.168",
+    },
+    user_defined: {
+      udf1: {
+        user_id: "59685578-98ab-4fa3-8aaf-f4907a2c5d69",
+        trans_id: "e49598c6-4377-46ea-8934-d269d232ddb8",
+      }
     },
     payment_token: req.body.token,
     return: process.env.PAYTAB_REDIRECT_URL, // pick from env, browser redirect url afer success of 3ds
-    callback:process.env.PAYTAB_CALLBACK_URL // post request from paytab server to our server with transaction details 
-  }
+    callback: process.env.PAYTAB_CALLBACK_URL, // post request from paytab server to our server with transaction details
+  };
 
-  if(req.body.saveCard==true)
-  {
-    payload.tokenise=2;
+  if (req.body.saveCard == true) {
+    payload.tokenise = 2;
     // payload.tran_class='recurring';
   }
 
@@ -41,28 +46,29 @@ router.post("/payment", async function (req, res, next) {
       "https://secure.paytabs.sa/payment/request",
       payload,
       {
-        headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
+        headers: { authorization: process.env.PAYTAB_SECRET_KEY }, //Secret key
       }
     );
-    res.send({message:"Success",result:trns.data,statusCode:200});
+    res.send({ message: "Success", result: trns.data, statusCode: 200 });
   } catch (error) {
-    res.status(400).send({message:"Failure",result:error,statusCode:400});
+    res
+      .status(400)
+      .send({ message: "Failure", result: error, statusCode: 400 });
     console.log("error", error);
   }
-  
 });
 
 router.post("/updateTransactionStatus", async function (req, res, next) {
   // Update the complete transaction details in db mark it as approved/denied state  according to the response & values in user defined fields
   // If card is tokenized save it into database the token,the hidden card number referencing the user id
-  console.log('updated transaction output',req.body);
+  console.log("updated transaction output", req.body);
   res.status(200).send();
 });
 
 router.post("/hosted-payment", async function (req, res, next) {
   // Store initial transaction details in db mark it as pending state
   // Add user id & own db transaction id into user defined field
-  let payload={
+  let payload = {
     profile_id: process.env.PAYTAB_PROFILE_ID,
     cart_amount: req.body.amount,
     cart_currency: "SAR",
@@ -81,55 +87,61 @@ router.post("/hosted-payment", async function (req, res, next) {
       city: "Dubai",
       state: "DU",
       country: "AE",
-      ip: "91.74.146.168",  
+      ip: "91.74.146.168",
     },
-    user_defined:{
-      udf1:"user1",
-      udf2:"trans1"
+    user_defined: {
+      udf1: {
+        user_id: "59685578-98ab-4fa3-8aaf-f4907a2c5d69",
+        trans_id: "e49598c6-4377-46ea-8934-d269d232ddb8",
+      }
     },
     return: `${process.env.PAYTAB_REDIRECT_URL}`, // pick from env, browser redirect url afer success of 3ds
-    callback:process.env.PAYTAB_CALLBACK_URL // post request from paytab server to our server with transaction details 
-  }
-  console.log('py',payload)
+    callback: process.env.PAYTAB_CALLBACK_URL, // post request from paytab server to our server with transaction details
+  };
+  console.log("py", payload);
   try {
     let trns = await axios.post(
       "https://secure.paytabs.sa/payment/request",
       payload,
       {
-        headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
+        headers: { authorization: process.env.PAYTAB_SECRET_KEY }, //Secret key
       }
     );
-    res.send({message:"Success",result:trns.data,statusCode:200});
+    res.send({ message: "Success", result: trns.data, statusCode: 200 });
   } catch (error) {
-    res.status(400).send({message:"Failure",result:error,statusCode:400});
+    res
+      .status(400)
+      .send({ message: "Failure", result: error, statusCode: 400 });
     console.log("error", error);
   }
 });
 
-router.post("/transaction-details",async function(req,res,next){
-  let payload={
+router.post("/transaction-details", async function (req, res, next) {
+  let payload = {
     profile_id: process.env.PAYTAB_PROFILE_ID,
-    tran_ref:req.body.tran_ref 
-  }
-  console.log('py',payload)
+    tran_ref: req.body.tran_ref,
+  };
+  console.log("py", payload);
   try {
     let trns = await axios.post(
       "https://secure.paytabs.sa/payment/query",
       payload,
       {
-        headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
+        headers: { authorization: process.env.PAYTAB_SECRET_KEY }, //Secret key
       }
     );
-    res.send({message:"Success",result:trns.data,statusCode:200});
+    res.send({ message: "Success", result: trns.data, statusCode: 200 });
   } catch (error) {
-    res.status(400).send({message:"Failure",result:error,statusCode:400});
+    res
+      .status(400)
+      .send({ message: "Failure", result: error, statusCode: 400 });
     console.log("error", error);
   }
-})
+});
 
-router.post("/refund",async function(req,res,next){
+router.post("/refund", async function (req, res, next) {
   // Fetch other details on the basis of transfer reference  from database
-  let payload={
+  let payload = {
     profile_id: process.env.PAYTAB_PROFILE_ID,
     tran_type: "refund",
     tran_class: "ecom",
@@ -137,24 +149,24 @@ router.post("/refund",async function(req,res,next){
     cart_currency: "SAR",
     cart_amount: 500,
     cart_description: "Booking refund due to cancellation",
-    tran_ref: req.body.tran_ref
-  }
-  console.log('py',payload)
+    tran_ref: req.body.tran_ref,
+  };
+  console.log("py", payload);
   try {
     let trns = await axios.post(
       "https://secure.paytabs.sa/payment/request",
       payload,
       {
-        headers: { authorization: process.env.PAYTAB_SECRET_KEY}, //Secret key 
+        headers: { authorization: process.env.PAYTAB_SECRET_KEY }, //Secret key
       }
     );
-    res.send({message:"Success",result:trns.data,statusCode:200});
+    res.send({ message: "Success", result: trns.data, statusCode: 200 });
   } catch (error) {
-    res.status(400).send({message:"Failure",result:error,statusCode:400});
+    res
+      .status(400)
+      .send({ message: "Failure", result: error, statusCode: 400 });
     console.log("error", error);
   }
-})
-
-
+});
 
 module.exports = router;
